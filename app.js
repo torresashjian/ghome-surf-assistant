7c7ab0fb9a6b28ef3921d009d8ca95b015ed93b9
Miguel Torres <migueltorreslopez@gmail.com>
2017-01-10 15:16:13 -0800

    Add action

---
+++ app.js
@@ -0,0 +1,70 @@
+// Copyright 2016, Google, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START app]
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+
+let ActionsSdkAssistant = require('actions-on-google').ActionsSdkAssistant;
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+app.set('port', (process.env.PORT || 8080));
+app.use(bodyParser.json({type: 'application/json'}));
+
+app.post('/', function (request, response) {
+  console.log('handle post');
+  const assistant = new ActionsSdkAssistant({request: request, response: response});
+
+  function mainIntent (assistant) {
+    console.log('mainIntent');
+    let inputPrompt = assistant.buildInputPrompt(true, '<speak>Hi! <break time="1"/> ' +
+          'I can read out an ordinal like ' +
+          '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>',
+          ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+    assistant.ask(inputPrompt);
+  }
+
+  function rawInput (assistant) {
+    console.log('rawInput');
+    if (assistant.getRawInput() === 'bye') {
+      assistant.tell('Goodbye!');
+    } else {
+	if(assistant.getRawInput() === '6'){
+	let inputPrompt = assistant.buildInputPrompt(true, '<speak>Hey, This is Lindseys favourite number</speak>',
+          ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+      assistant.ask(inputPrompt);	
+} else {
+      let inputPrompt = assistant.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">' +
+        assistant.getRawInput() + '</say-as></speak>',
+          ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+      assistant.ask(inputPrompt);
+    }}
+  }
+
+  let actionMap = new Map();
+  actionMap.set(assistant.StandardIntents.MAIN, mainIntent);
+  actionMap.set(assistant.StandardIntents.TEXT, rawInput);
+
+  assistant.handleRequest(actionMap);
+});
+
+// Start the server
+let server = app.listen(app.get('port'), function () {
+  console.log('App listening on port %s', server.address().port);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END app]
